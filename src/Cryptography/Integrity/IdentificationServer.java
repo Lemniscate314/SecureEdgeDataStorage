06ced4553f747bfdca6a2f0f4d3ecdae769a39c4
Issa MBOUP <issa.mboup314@gmail.com>
2024-01-03 07:58:44 +0100

    feat : add hash functions

--- src/Cryptography/Integrity/IdentificationServer.java
+++ src/Cryptography/Integrity/IdentificationServer.java
@@ -5,6 +5,8 @@ import it.unisa.dia.gas.jpbc.Field;
 import it.unisa.dia.gas.jpbc.Pairing;
 import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 public class IdentificationServer {
@@ -25,6 +27,39 @@ public class IdentificationServer {
     public Element generatePublicKey() {
         Element PKs = P.duplicate().mulZn(ts).getImmutable();
         return PKs;
+    }
+    private Element hashFunctionH1(String identity) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(identity.getBytes());
+
+            return G0.newElementFromHash(hash, 0, hash.length).getImmutable();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Element hashFunctionH2(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes());
+
+            return Zp.newElementFromBytes(hash).getImmutable();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private byte[] hashFunctionH3(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return digest.digest(input.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static void main(String[] args) {
